940c8defd2303908d60b75d992e3540ecca36f02
Tim <Tim280696@hotmail.com>
2022-04-28 11:27:51 +0200

    fixed

--- app.js
+++ app.js
@@ -21,28 +21,31 @@ app.use((req, res, next) => {
     next();
 });
 
+app.get('/api/products', (req, res, next) => {
+    Product.find().then(products => res.status(200).json({products})).catch(error => res.status(400).json({error}))
+});
+
+app.get('/api/products/:id', (req, res, next) => {
+    Product.findOne({_id: req.params.id}).then(product => res.status(200).json({product})).catch(error => res.status(404).json({error}))
+})
+
 app.post('/api/products', (req, res, next) => {
     delete req.body._id
     const product = new Product({
         ...req.body
     })
-    product.save().then(() => res.status(201).json({message: 'Produit enregistré !'})).catch( error => res.status(400).json({ error }))
+    product.save().then(() => res.status(201).json({ product })).catch( error => res.status(400).json({ error }))
 })
 
 app.put('/api/products/:id', (req, res, next) => {
-    Product.updateOne({_id: req.params.id}, {...req.body, _id: req.params.id}).then(res.status(200).json({message: "Produit modifié !"})).catch(res.status(400).json({error}))
+    Product.updateOne({_id: req.params.id}, {...req.body, _id: req.params.id}).then(res.status(200).json({ message: 'Modified!' })).catch(res.status(400).json({error}))
 })
 
 app.delete('/api/products/:id', (req, res, next) => {
-    Product.deleteOne({_id: req.params.id}, {...req.body, _id: req.params.id}).then(res.status(200).json({message: "Produit supprimé !"})).catch(res.status(400).json({error}))
+    Product.deleteOne({_id: req.params.id}, {...req.body, _id: req.params.id}).then(res.status(200).json({ message: 'Deleted!' })).catch(res.status(400).json({error}))
 })
 
-app.get('/api/products/:id', (req, res, next) => {
-    Product.findOne({_id: req.params.id}).then(product => res.status(200).json(product)).catch(error => res.status(404).json({error}))
-})
 
-app.get('/api/products', (req, res, next) => {
-    Product.find().then(products => res.status(200).json(products)).catch(error => res.status(400).json({error}))
-});
+
 
 module.exports = app
